47cdf81e0b08136b272d499698136010cbbab1ee
mounikavemunoori <106724079+mounikavemunoori@users.noreply.github.com>
2022-07-26 18:21:42 +0530

    create the new file name as containerID.sh

---
+++ selenum_automation_python/extract_containerID.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Hello world"
+ls
+VAR1=docker ps
+echo $VAR1
+BASEPATH=$(pwd)
+echo $BASEPATH
+
+SUBFOLD1=${BASEPATH%%/}/xlread1.xlsx
+SUBFOLD2=${BASEPATH%%/}/subFold2
+
+echo $SUBFOLD1
+
